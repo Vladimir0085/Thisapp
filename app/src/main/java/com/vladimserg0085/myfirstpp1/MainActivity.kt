ca484997393ac1784677bdcc47fe84ab48d9574f
vladimir0085 <vladimserg0085>
2021-11-24 20:33:54 +0700

    first commit

---
+++ app/src/main/java/com/vladimserg0085/myfirstpp1/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.vladimserg0085.myfirstpp1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
